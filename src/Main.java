da3f3c776b963c051ae6647ee9fe4e4bb29c8297
Hui Wagner <huiwagne@uw.edu>
2024-03-05 21:06:33 -0800

    Second Commit.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.security.SecureRandom;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -28,11 +29,12 @@ public class Main {
         // Keep prompting the dialogue until user enters the right name
         while (!isValidName) {
             System.out.println("""
-                    =============================================================================
+                    ================================================================================
                     Step 1: Please type your name includes your first name and last name:
-                     - Your first and last name has to in a range between 1 and 50 characters;
-                     - Your names can not include any numbers or special characters;
-                     - Use space to separate your first and last name.""");
+                          - Your first and last name has to in a range between 1 and 50 characters;
+                          - Your names can not include any numbers or special characters;
+                          - Use space to separate your first and last name.
+                     -------------------------------------------------------------------------------""");
 
             name = nameScanner.nextLine();
             String regex = "^[a-zA-Z]{1,50}\\s[a-zA-Z]{1,50}$";
@@ -42,9 +44,9 @@ public class Main {
             // If the pattern matches, prompt the message and store return the name
             if (nameMatch.matches()) {
                 isValidName = true;
-                System.out.println("<" + name + ">" + " is a valid name");
+                System.out.println("<" + name + ">" + " is a valid name.");
             } else {
-                System.out.println("<" + name + ">" + " is NOT a valid first and last name, please try again");
+                System.out.println("<" + name + ">" + " is NOT a valid first and last name, please try again.");
             }
         }
         return name;
@@ -53,7 +55,7 @@ public class Main {
 
     public static int[] numChecking() {
         // The regular expression for checking the int numbers
-        String regex = "^((-?[1-9]\\d{0,2})((,?\\d{3})+))|0$";
+        String regex = "^-?\\d{1,3}(,\\d{3})*$";
         Pattern pattern = Pattern.compile(regex);
 
         int numberOne = 0;
@@ -65,10 +67,10 @@ public class Main {
         // Keep prompting the dialogue until user enters a valid number
         while (!isValidNum1) {
             System.out.println("""
-                    ======================================================================================
-                    Step 2: Please type in two int values step by step (range between -2,147,483,647 to 2,147,483,647):
-                     - Please type your FIRST int value:
-                    """);
+                    ===================================================================================================
+                    Step 2: Please type in two int values step by step (range between -2,147,483,648 to 2,147,483,647):
+                          - Please type your FIRST int value:
+                    ---------------------------------------------------------------------------------------------------""");
             Scanner num1Scanner = new Scanner(System.in);
             String num1 = num1Scanner.nextLine();
 
@@ -95,7 +97,10 @@ public class Main {
         }
 
         while (!isValidNum2) {
-            System.out.println(" - Please type your SECOND int value:");
+            System.out.println("""
+                     ===========================================
+                          - Please type your SECOND int value:
+                    --------------------------------------------""");
             Scanner num2Scanner = new Scanner(System.in);
             String num2 = num2Scanner.nextLine();
 
@@ -129,9 +134,9 @@ public class Main {
         while (!isValidInput) {
             System.out.println("""
                 ======================================================================================
-                Please input a valid name for your INPUT File:
-                 - The file name can't include: <, >, :, ", /, \\, |, ?, *
-                """);
+                Step 3: Please input a valid name for your INPUT File:
+                      - The file name can't include: <, >, :, ", /, \\, |, ?, *
+                ----------------------------------------------------------------------------""");
             Scanner inputScanner = new Scanner(System.in);
             inputFileName = inputScanner.nextLine();
 
@@ -160,9 +165,9 @@ public class Main {
         while (!isValidOutput) {
             System.out.println("""
                 ======================================================================================
-                Please input a valid name for your OUTPUT File:
-                 - The file name can't include: <, >, :, ", /, \\, |, ?, *
-                """);
+                Step 4: Please input a valid name for your OUTPUT File:
+                      - The file name can't include: <, >, :, ", /, \\, |, ?, *
+                ----------------------------------------------------------------------------""");
             Scanner outputScanner = new Scanner(System.in);
             outputFileName = outputScanner.nextLine();
 
@@ -185,6 +190,33 @@ public class Main {
     }
 
     public static void handlePassword () {
+
+        System.out.println("""
+                ================================================================
+                Step 5: Create a password that follow the rules below:
+                      - Your password has to in between 8 to 255 characters
+                      - At least 1 upper case letter
+                      - At least 1 lower case letter
+                      - At least 1 number
+                      - At least 1 special letter
+                ---------------------------------------------------------------""");
+
+        Scanner passWordScanner = new Scanner(System.in);
+        String password = passWordScanner.nextLine();
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$\n";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        boolean isValidPS = matcher.matches();
+
+        if (password.isEmpty()) {
+            System.out.println("You entered an empty password, try again");
+        }
+
+
+
+        SecureRandom secureRandom = SecureRandom()
+
 
     }
 
